ea349cc4e8aea9f6b449ba36bcfd43b08951a8ee
ilafper <ivanaalfaya@gmail.com>
2026-03-12 18:55:21 +0100

    api agenda recu en proceso, esta hecho para mongo, crear usuario, borrar, listar y busqueda para nombre

--- api/app.js
+++ api/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const { MongoClient, ServerApiVersion } = require('mongodb');
 const { ObjectId } = require('mongodb');
+
+const { z, regex } = require('zod');
 const app = express();
 app.use(express.json());
 
@@ -17,9 +19,9 @@ async function connectToMongoDB() {
   try {
     await client.connect();
     console.log("ESta conectado, Go go go go");
-    const db = client.db('MaquinaElectron');
+    const db = client.db('Agenda_Recu');
     return {
-      productos: db.collection('producto'),
+      clientes: db.collection('clientes'),
     };
   } catch (error) {
     console.error("Error al conectar a MongoDB:", error);
@@ -27,58 +29,188 @@ async function connectToMongoDB() {
   }
 }
 
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  next();
-});
 
 
-//Obtener todos los PRoductos
-
-
-app.get('/api/productos', async (req, res) => {
+app.get('/api/clientes', async (req, res) => {
   try {
-    const { productos } = await connectToMongoDB();
-    const lista_products = await productos.find().toArray();
-    res.json(lista_products);
+    const { clientes } = await connectToMongoDB();
+    const lista_clientes = await clientes.find().toArray();
+    console.log(lista_clientes);
+    
+    res.json({ success: true,mensaje:"todos los clientes Sisi" , lista_clientes});
   } catch (error) {
     res.status(500).json({ error: 'Error al obtener los mangas' });
   }
 });
 
-//Crear productos
-app.post('/api/crearproducto', async (req, res) => {
-  const nuevoProducto = req.body;
+//Crear cliente 
+app.post('/api/crearcliente', async (req, res) => {
   try {
-    const { productos } = await connectToMongoDB();
-    const resultado = await productos.insertOne(nuevoProducto);
-    console.log(`PRODUCTO creado con ID: ${resultado.insertedId}`);
-    res.status(201).json({ mensaje: "Producto creado", id: resultado.insertedId });
+    // recpger los datos 
+    const {nombre, apellidos,telefono,direccion,correo} = req.body;
+
+    const { clientes } = await connectToMongoDB();
+    // crear el esquema para validar con zod
+    const resultado = z.object({
+      nombre: z.string().min(3, "El nombre debe tener al menos 3 caracteres"),
+      apellidos: z.string().min(3, "Los apellidos deben tener al menos 3 caracteres"),
+      //valedar en el tlf no meter letras y un minimo y maximo
+      telefono: z.string() .regex(/^\d{7,15}$/, "El teléfono debe contener solo números (7-15 dígitos)").min(7,"minimo 7 digitos").max(15,'maximo 15 digitos'),
+      correo: z.email("Correo electrónico inválido"),
+      direccion:z.string().min(5, "la direccion debe estar correcta ").max(100, 'la longitud no puede ser mayor que 100 caracteres'),
+      
+    }).safeParse({nombre, apellidos,telefono,direccion,correo});
+
+    // coger el mensaje de error para mostarar
+     if (!resultado.success) {
+      console.log("sisis zod");
+      //resultado.error?.issues?.[0]?.message;
+      const primerError = resultado.error?.issues?.[0]?.message;;
+      console.log(primerError);
+      
+      return res.status(400).json({
+        success: false,
+        message: primerError  
+      });
+    }
+
+
+    // Verificar correo duplicado
+    const usuarioExistente = await clientes.findOne({ correo });
+    
+    if (usuarioExistente) {
+      return res.status(409).json({ 
+        success: false,
+        message: "El correo ya está registrado" 
+      });
+    }
+
+
+    const code_user = 'codigo' + Math.floor(Math.random() * 1000);
+    // nuevo user
+    
+    const nuevoUsuario = {
+      nombre,
+      apellidos,
+      telefono,
+      direccion,
+      correo,
+      code_user
+    };
+
+
+    console.log("nuevo nuevo",nuevoUsuario);
+    //insertar en l base de datos 
+    await clientes.insertOne(nuevoUsuario);
+
+    // mensaje final correcto si fue bien
+    res.status(201).json({
+      success: true,
+      message: "Usuario creado exitosamente",
+      user: {
+        nombre,
+        apellidos,
+        telefono,
+        direccion,
+        correo,
+        code_user
+      }
+    });
+
+    
   } catch (error) {
-    console.error("Error al guardar el producto en MongoDB:", error);
+    console.error("Error al guardar el cliente", error);
     res.status(500).json({ error: 'Error interno del servidor al crear el producto' });
   }
 
 });
 
-//eliminar producto por id
-app.delete('/api/delete/:id', async (req, res) => {
-  //recivir la url
-  const { id } = req.params;
-  try {
-    const { productos } = await connectToMongoDB();
-    const resultado = await productos.deleteOne({ _id: new ObjectId(id) });
 
-    if (resultado.deletedCount === 0) {
-      return res.status(404).json({ mensaje: 'Producto no encontrado' });
+
+
+ //eliminar producto por id
+app.delete("/api/eliminarcliente/:id", async (req, res) => {
+  const { clientes } = await connectToMongoDB();
+
+  try {
+    const id_eliminar = req.params.id;
+
+    console.log(id_eliminar);
+    
+    // borrar el borrar y su e vento
+    const resultado = await clientes.deleteOne({ code_user: id_eliminar});
+
+ 
+    if (resultado.deletedCount === 0 ) {
+      return res.status(404).json({success:false, error: "No se encontro el cliente" });
     }
 
-    res.json({ mensaje: 'Producto eliminado correctamente' });
+    res.json({success:true, mensaje: "cliente con id: " + id_eliminar + "eliminado correctamente" });
+
   } catch (error) {
-    console.error("Error al eliminar el producto:", error);
-    res.status(500).json({ error: 'Error interno del servidor al eliminar el producto' });
+    res.status(500).json({ error: "Error al eliminar", detalle: error.message });
+  }
+
+});
+
+
+
+app.get("/api/filtronombre/:nombreBusqueda", async (req, res) => {
+  const { clientes } = await connectToMongoDB();
+
+  try {
+    const nombreBusqueda = req.params.nombreBusqueda; 
+    console.log("Buscando nombre:", nombreBusqueda);
+
+    if (!nombreBusqueda) {
+      return res.status(400).json({ success: false, error: "Debes enviar un nombre" });
+    }
+
+    // busqueda parcial del campo nombre
+    const filtroNombre = await clientes.find({ nombre: new RegExp(nombreBusqueda, 'i') }).toArray();
+    console.log(filtroNombre);
+
+    if (filtroNombre.length=== 0 ) {
+      console.log("no hay coincidencias");
+      return res.json({ success: false, error:"no hay coincidencias"});
+    }
+
+
+    res.json({ success: true, mensaje:"todas las coincidencias", datos: filtroNombre });
+
+  } catch (error) {
+    res.status(500).json({ success: false, error: "Error al encontrar", detalle: error.message });
+  }
+});
+
+
+//filtro apellidos
+
+app.get("/api/filtroapellidos/:apeBusqueda", async (req, res) => {
+  const { clientes } = await connectToMongoDB();
+
+  try {
+    const apeBusqueda = req.params.apeBusqueda; 
+    console.log("Buscando nombre:", apeBusqueda);
+
+    if (!apeBusqueda) {
+      return res.status(400).json({ success: false, error: "Debes enviar un apellido" });
+    }
+
+    // busqueda parcial del campo nombre
+    const filtroapellidos = await clientes.find({ apellidos: new RegExp(apeBusqueda, 'i') }).toArray();
+    console.log(filtroapellidos);
+
+    if (filtroapellidos.length=== 0 ) {
+      console.log("no hay coincidencias");
+      return res.json({ success: false, error:"no hay coincidencias"});
+    }
+
+
+    res.json({ success: true, mensaje:"todas las coincidencias", datos: filtroapellidos });
+
+  } catch (error) {
+    res.status(500).json({ success: false, error: "Error al encontrar", detalle: error.message });
   }
 });
 
@@ -86,27 +218,31 @@ app.delete('/api/delete/:id', async (req, res) => {
 
 
 
-// Actualizar producto por ID
-app.put("/api/actuproducto/:id", async (req, res) => {
-    const { id } = req.params; 
-    const datosActualizados = req.body;
-    try {
-        const { productos } = await connectToMongoDB();
-        const resultado = await productos.updateOne(
-            { _id: new ObjectId(id) }, 
-            { $set: datosActualizados }
-        );
 
-        if (resultado.matchedCount === 0) {
-            return res.status(404).json({ error: "Producto no encontrado" });
-        }
 
-        res.json({ mensaje: "Producto actualizado ISISISIS" });
-    } catch (error) {
-        console.error("NONNNONONO", error);
-        res.status(500).json({ error: "Error servidor nONONONO" });
-    }
-});
+
+
+// // Actualizar producto por ID
+// app.put("/api/actuproducto/:id", async (req, res) => {
+//     const { id } = req.params; 
+//     const datosActualizados = req.body;
+//     try {
+//         const { productos } = await connectToMongoDB();
+//         const resultado = await productos.updateOne(
+//             { _id: new ObjectId(id) }, 
+//             { $set: datosActualizados }
+//         );
+
+//         if (resultado.matchedCount === 0) {
+//             return res.status(404).json({ error: "Producto no encontrado" });
+//         }
+
+//         res.json({ mensaje: "Producto actualizado ISISISIS" });
+//     } catch (error) {
+//         console.error("NONNNONONO", error);
+//         res.status(500).json({ error: "Error servidor nONONONO" });
+//     }
+// });
 
 
 
